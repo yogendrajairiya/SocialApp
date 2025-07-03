bbe210cf2b122e0b01ce9c541e4e8f23b9b04e45
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-07-03 17:31:16 +0530

    Added view for tweet application operations

--- socialchai/tweet/views.py
+++ socialchai/tweet/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_list_or_404
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')  # Ensure you have a template at 'tweet/index.html'
+  
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')  # Fetch all tweets ordered by creation date
+    return render(request, 'tweet/tweet_list.html', {'tweets': tweets})
+  
+def tweet_create(request):
+  if request.method == 'POST':
+      form = TweetForm(request.POST, request.FILES)  # Handle file uploads
+      if form.is_valid():
+          tweet = form.save(commit=False)
+          tweet.user = request.user  # Associate the tweet with the logged-in user
+          tweet.save()
+          return redirect('tweet_list')  
+  else:
+      form = TweetForm()
+  
+  return render(request, 'tweet_form.html', {'form': form})  # Render the form for creating a new tweet
+
+def tweet_edit(request, tweet_id):
+    tweet = get_list_or_404(Tweet, id=tweet_id)
+    if request.method == 'POST':
+      form = TweetForm(request.POST, request.FILES, instance=tweet)  # Handle file uploads
+      if form.is_valid():
+          form.save()
+          return redirect('tweet_list')  # Redirect to the tweet list after editing
+    else:
+      form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': form})  
+    
+def tweet_delete(request, tweet_id):
+    tweet = get_list_or_404(Tweet, id=tweet_id, user = request.user)  
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list') 
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})  
