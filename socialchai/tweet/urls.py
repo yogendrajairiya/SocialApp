0a49a3e9f04b4fb4f4bce2417da9f573eb1a45ad
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-07-03 13:15:56 +0530

    Initialize social chai project

---
+++ socialchai/tweet/urls.py
@@ -0,0 +1,10 @@
+#  BASE_DIR = Path(__file__).resolve().parent.parent
+# from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+# from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), 
+] 
