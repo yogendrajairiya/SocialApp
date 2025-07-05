b9fd832bb60d4a2747f5da29e536ba8b13709e61
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-07-05 23:51:55 +0530

    Added user registration functionality and modified tweet views and templates to include user authentication features.

--- socialchai/socialchai/urls.py
+++ socialchai/socialchai/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),  # Include the tweet app's URLs
-    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('accounts/', include('django.contrib.auth.urls')),  # For login, logout, password management
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Serve media files
 
--- socialchai/tweet/forms.py
+++ socialchai/tweet/forms.py
@@ -4,20 +4,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
-  class Meta:
-    model = Tweet
-    fields = ['text', 'photo']  # fields to be displayed in the form
-
-class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user   
+        model = Tweet
+        fields = ['text', 'photo']
+        
+class UserRegistrationForm(UserCreationForm):
+  email = forms.EmailField(required=True)
+  class Meta:
+    model = User
+    fields = ('username', 'email', 'password1', 'password2')
+  
+  def save(self, commit=True):
+    user = super().save(commit=False)
+    user.email = self.cleaned_data['email']
+    if commit:
+      user.save()
+    return user
