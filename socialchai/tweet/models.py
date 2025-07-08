5af9d9b89de293cedbe96a632ecba6002c0d23a9
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-07-08 21:56:39 +0530

    Added comment model data

--- socialchai/tweet/models.py
+++ socialchai/tweet/models.py
@@ -11,3 +11,13 @@ class Tweet(models.Model):
 
     def __str__(self):
         return f"{self.user.username}: {self.text[:10]}..."  # Display first 20 characters of the tweet content
+    
+class Comment(models.Model):
+    tweet = models.ForeignKey(Tweet, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=60)
+    attached_photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.user.username} on {self.tweet.id}: {self.text[:10]}..."  # Display first 20 characters of the comment content
