d06a98636c409aed722ba71f955abd63037b7fc4
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-07-24 15:40:33 +0530

    Added my profile and edit section and partial page

--- socialchai/socialchai/settings.py
+++ socialchai/socialchai/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tweet',  # Your app name
-    'rest_framework'
+    'rest_framework',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'socialchai.urls'
